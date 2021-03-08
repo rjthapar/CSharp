fabe10e1b276db84d0914aa912c5817451ffa232
rajat.thapar <rajat.thapar@outlook.com>
2021-03-08 18:22:22 +0530

    Minimum Difference Between Largest and Smallest Value in Three Moves

---
+++ MinDifference.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algotest
+{
+    /// <summary>
+    /// Minimum Difference Between Largest and Smallest Value in Three Moves
+    /// </summary>
+    class MinDifference
+    {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("Enter length of array:-");
+            int length = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter {0} values", length);
+            int[] arr = new int[length];
+            for (int i = 0; i < length; i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine(Solution.MinDifference(arr));
+        }
+    }
+        public static class Solution
+        {
+            public static int MinDifference(int[] nums)
+            {
+                if (nums.Length <= 4)
+                    return 0;
+                Array.Sort(nums);
+                int result = int.MaxValue;
+
+                for (int i = 0; i < 4; i++)
+                {
+                    result = Math.Min(result, (nums[nums.Length - 1 - 3 + i] - nums[i]));
+                }
+                return result;
+            }
+        }
+    
+}
