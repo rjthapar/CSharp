eb7f077da9c580acd2d556516b6664be3f27f450
rajat.thapar <rajat.thapar@outlook.com>
2021-03-08 16:17:13 +0530

    using HashSet in C# to remove duplicate values in a String

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Algotest
+{
+    class Program
+    {
+        /// <summary>
+        /// using HashSet in C# to remove duplicate values in a String
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string whose duplicate values need to be removed:-");
+            string inputString = Console.ReadLine();
+            char[] inputCharValues = new char[inputString.Length];
+            inputCharValues = inputString.ToCharArray();
+            HashSet<char> hSet = new HashSet<char>();
+            foreach(Char c in inputCharValues)
+            {
+                hSet.Add(c);
+            }
+            
+            foreach(char val in hSet)
+            {
+                Console.Write(val);
+            }
+
+        }
+    }
+}
